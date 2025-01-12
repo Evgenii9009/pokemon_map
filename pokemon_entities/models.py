4307b02331aeee88eda23f22b07971622f9f3100
Eugene Fedorov <uptiik02@gmail.com>
2025-01-12 18:43:45 +0300

    summary+

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -8,25 +8,25 @@ class Pokemon(models.Model):
     title_en = models.CharField('название на английском', max_length=200, blank=True)
     title_jp = models.CharField('название на японском', max_length=200, blank=True)
     description = models.TextField('описание', blank=True)
-    previous_evolution = models.ForeignKey("self", null=True, blank=True, on_delete=models.SET_NULL, related_name='next_evolution', verbose_name='из кого эволюционирует')
+    previous_evolution = models.ForeignKey("self", null=True, blank=True, on_delete=models.SET_NULL, related_name='next_evolutions', verbose_name='из кого эволюционирует')
 
     def __str__(self):
-        return f'{self.title}'
+        return {self.title}
     
 
 class PokemonEntity(models.Model):
     """Покемон на карте"""
     lon = models.FloatField('долгота', null=True)
     lat = models.FloatField('широта', null=True)
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='покемон')
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='покемон', related_name='pokemons')
     appeared_at = models.DateTimeField('время появления', null=True)
     disappeared_at = models.DateTimeField('время исчезновения', null=True)
-    Level = models.IntegerField('уровень', null=True, blank=True)
-    Health = models.IntegerField('здоровье', null=True, blank=True)
-    Strength = models.IntegerField('сила', null=True, blank=True)
-    Defence = models.IntegerField('защита', null=True, blank=True)
-    Stamina = models.IntegerField('выносливость', null=True, blank=True)
+    level = models.IntegerField('уровень', null=True, blank=True)
+    health = models.IntegerField('здоровье', null=True, blank=True)
+    strength = models.IntegerField('сила', null=True, blank=True)
+    defence = models.IntegerField('защита', null=True, blank=True)
+    stamina = models.IntegerField('выносливость', null=True, blank=True)
 
 
     def __str__(self):
-        return f'{self.pokemon}'
+        return {self.pokemon}
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,8 +1,8 @@
 import folium
-import json
 
-from django.http import HttpResponseNotFound
+
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import Pokemon, PokemonEntity
 from django.utils.timezone import localtime
 
@@ -27,9 +27,13 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
         icon=icon,
     ).add_to(folium_map)
 
+def check_image(pokemon):
+    if pokemon.image:
+        return pokemon.image.url
 
 def show_all_pokemons(request):
-    map_pokemons = PokemonEntity.objects.filter(disappeared_at__gt=localtime(), appeared_at__lt=localtime())
+    now = localtime()
+    map_pokemons = PokemonEntity.objects.filter(disappeared_at__gt=now, appeared_at__lt=now)
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon_entity in map_pokemons:
         add_pokemon(
@@ -39,11 +43,11 @@ def show_all_pokemons(request):
         )
 
     pokemons_on_page = []
-    page_pokemons = Pokemon.objects.all()
+    page_pokemons = Pokemon.objects.all()  
     for pokemon in page_pokemons:
-        pokemons_on_page.append({'img_url': pokemon.image.url, 'pokemon_id': pokemon.id, 'title_ru': pokemon.title}) if pokemon.image else pokemons_on_page.append({'img_url': "", 'pokemon_id': pokemon.id, 'title_ru': pokemon.title})
-
-
+        pokemons_on_page.append({'img_url': check_image(pokemon),
+                                 'pokemon_id': pokemon.id,
+                                 'title_ru': pokemon.title})
     return render(request, 'mainpage.html', context={
         'map': folium_map._repr_html_(),
         'pokemons': pokemons_on_page,
@@ -51,10 +55,7 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    try:
-        needed_pokemon = Pokemon.objects.get(id=pokemon_id)
-    except:
-        return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
+    needed_pokemon = get_object_or_404(Pokemon, id=pokemon_id)
     pokemon_on_page = {'img_url': needed_pokemon.image.url, 
                        'title_ru': needed_pokemon.title, 
                        'title_en': needed_pokemon.title_en, 
@@ -64,15 +65,13 @@ def show_pokemon(request, pokemon_id):
         pokemon_on_page["previous_evolution"] = {"pokemon_id": needed_pokemon.previous_evolution.id,
                                                  "img_url": needed_pokemon.previous_evolution.image.url,
                                                  "title_ru": needed_pokemon.previous_evolution.title
-                                                }
-    try:    
-        next_evolution = needed_pokemon.next_evolution.get()
+                                                }  
+    next_evolution = needed_pokemon.next_evolutions.first()
+    if next_evolution:
         pokemon_on_page['next_evolution'] = {"pokemon_id": next_evolution.id,
-                                            "img_url": next_evolution.image.url,
-                                            "title_ru": next_evolution.title
+                                             "img_url": next_evolution.image.url,
+                                             "title_ru": next_evolution.title
                                             }
-    except Pokemon.DoesNotExist:
-        pass
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     map_pokemons = PokemonEntity.objects.filter(pokemon=pokemon_id)
     for pokemon_entity in map_pokemons:
